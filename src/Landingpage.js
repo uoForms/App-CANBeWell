df4006dd43cb3c75a2fc8944b5f452b65609278e
Yuxing Jiang <yjian118@uottawa.ca>
2020-01-08 13:47:21 -0500

    modified the logo size to fit the view and tidy the landingpage

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -2,22 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import App from './App';
 
+import './LandingPage.css';
 import calllogo from './assets/Logos/logo_03-02.png';
+import { app } from 'firebase';
 
 
 class LandingPage extends React.Component {
-
     constructor(props) 
     {
-
         super(props);
      this.state=
      {
        language:""
      }
   this.handleChange= this.handleChange.bind(this);
-  this.handleChange2= this.handleChange2.bind(this);
-  
+  this.handleChange2= this.handleChange2.bind(this);  
     }
     handleChange(){
         this.setState({language:"english"})
@@ -34,41 +33,31 @@ class LandingPage extends React.Component {
    console.log("test:"+localStorage.getItem("app_language"));
    console.log("test:"+this.state.language);
       return (
-
-            <div>
-           
+        <div>          
            {this.state.language =="" || null ?(
-                   <div>
-                     <h3>Do not use this app for health advice.<br /></h3>
-                     <h5>It is still under development.<br />
-Ask your health care provider for all health advice.<br />
-<br /></h5>
-
-<h3>Ne pas utiliser cette application pour des conseils de santé.<br /></h3>
-<h5>L’appli  est encore en développement.<br />
-Veuillez consulter votre professionnel de la santé.</h5>
-  <div><img  src={calllogo} alt="CANBeWellLogo"/></div>
-        
-        <div class="langButton">
-   
-            
-                <button class="langButtonStyle" onClick={this.handleChange} >English</button>
-            
-               <button class="langButtonStyle" onClick={this.handleChange2}>Francais</button>
-           
-            </div>
-                   </div>
-                 ):(
-
-                   <div>
-                    <App appLanguage={this.state.language}/> 
-                                      </div>
-                 )}
-               
-             </div>
-         
-      );
-   
+              <div>
+                <h3>Do not use this app for health advice.<br /></h3>
+                <h5>It is still under development.<br />
+                    Ask your health care provider for all health advice.<br />
+                <br /></h5>
+                <h3>Ne pas utiliser cette application pour des conseils de santé.<br /></h3>
+                <h5>L’appli  est encore en développement.<br />
+                  Veuillez consulter votre professionnel de la santé.</h5>
+              <div>
+                <img class="landing-logo" src={calllogo} alt="CANBeWellLogo"/>
+              </div>       
+              <div className="langButton">           
+               <button class="langButtonStyle" onClick={this.handleChange} >English</button>           
+               <button class="langButtonStyle" onClick={this.handleChange2}>Francais</button>           
+              </div>
+          </div>
+            ):(
+                <div>
+                  <App appLanguage={this.state.language}/> 
+                </div>
+              )}
+        </div>         
+      );   
 }}
 
 
