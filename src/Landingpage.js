67ee88ee93584352d92417b8dee346ed970d3bb6
Yuxing Jiang <yjian118@uottawa.ca>
2020-03-23 14:31:45 -0400

    move to production evironment

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -27,7 +27,7 @@ class LandingPage extends React.Component {
     localStorage.setItem("app_language", this.state.language);
   }
   componentDidMount() {
-    initGA('UA-151893001-2');
+    initGA('UA-151893001-1');
   }
 
   render() {
