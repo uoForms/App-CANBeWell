01e8db1845b7d9e3ace048842c58648e1346a3ab
RISHANG SHARMA <59984749+RISHANGSHARMA@users.noreply.github.com>
2020-06-16 12:50:31 -0400

    listing try1
    added the listing but problem with +

--- src/Body/BodyModal.js
+++ src/Body/BodyModal.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { PageViewTimer, GaUserEvent } from '../Tracking';
+
 
 class BodyModal extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      topicDisplayed: []
+      topicDisplayed: [],
+      subject:[]
     };
   }
+  getTopicNAme=(display)=>{
+    var topicsToDisplay = display;
 
-  getsubjectArray = (display) => {
-    const Image = "./";
-    var subjectArray = [];
-    //kind of redendant sorry
+    var subjectArray=[];
+    
     var topicsToDisplay = display;
 
     topicsToDisplay.forEach((topic) => {
@@ -22,9 +25,9 @@ class BodyModal extends React.Component {
       subjectArray.push(<div><h3>{topic.name}</h3></div>);
       var k = 0;
       bodys.forEach((body) => {
-        var bodyArray = body.text.split(/(\[\[|\]\]|\n)/g);
+        //var bodyArray = body.text.split(/(\[\[|\]\]|\n)/g);
         var subject = body.subject.split(/(\[\[|\]\]|\n)/g);
-        var bodyArrayToDisplay = [];
+        //var bodyArrayToDisplay = [];
         var subjectArrayToDisplay = [];
         var outerTextToDisplay = [];
 
@@ -66,6 +69,73 @@ class BodyModal extends React.Component {
             subjectArrayToDisplay.push(subject[i]);
           }
         }
+        subjectArray.push(<div className="topicBody"><p><b>{subjectArrayToDisplay}<br/></b></p>{outerTextToDisplay}</div>);
+      });
+    });
+    return subjectArray;
+
+
+
+    //return topicHeading;
+  }
+
+
+  getsubjectArray = (display) => {
+    const Image = "./";
+    var subjectArray = [];
+    //kind of redendant sorry
+    var topicsToDisplay = display;
+
+    topicsToDisplay.forEach((topic) => {
+      var bodys = topic.body;
+      //var subject = body.subject;
+      //subjectArray.push(<div><h3>{topic.name}</h3></div>);
+      var k = 0;
+      bodys.forEach((body) => {
+        var bodyArray = body.text.split(/(\[\[|\]\]|\n)/g);
+        var subject = body.subject.split(/(\[\[|\]\]|\n)/g);
+        var bodyArrayToDisplay = [];
+       // var subjectArrayToDisplay = [];
+        var outerTextToDisplay = [];
+
+        // for (var i = 0; i < subject.length; i++) {
+        //   if (subject[i] == '[[') {
+        //     var link = subject[i + 1].split(';');
+        //     if (link[1] === undefined) {
+        //       link[1] = "undefined";
+        //     }
+        //     //try{
+        //     if (link[0] === "image" || link[0] === "images") {
+        //       var adress = Image + link[1].trim();
+        //       subjectArrayToDisplay.push(<div><img className="imageFromFolder" src={adress} alt="" /></div>);
+        //     }
+        //     /*else if(link[1].indexOf("topic") === 0 || link[1].indexOf("topic") === 1){
+        //       var id = topic.name + k;
+        //       k++;
+        //       var mTopic = link[1].replace("topic://", "").trim();
+        //       var outerText = this.getOuterText(mTopic);
+        //       subjectArrayToDisplay.push(<div onClick={(idTarget) => this.togglePopUp(id)}><font color="Yellow">{link[0]}</font><div id={id} className="popup"><span className="popuptext"><p>{outerText}</p></span></div></div>);
+        //     }
+        //     else if(link[1].indexOf("test") === 0 || link[1].indexOf("test") === 1){
+        //       var testId = topic.name + k;
+        //       k++;
+        //       var mTest = link[1].replace("test://", "").trim();
+        //       var testOuterText = this.getOuterText(mTest);
+        //       subjectArrayToDisplay.push(<div onClick={(idTarget) => this.togglePopUp(testId)}><font color="Yellow">{link[0]}</font><div id={testId} className="popup"><span className="popuptext"><p>{testOuterText}</p></span></div></div>);
+        //     }*/
+        //     else {
+        //       subjectArrayToDisplay.push(<a href={link[1]} target="_blank"><font color="Yellow">{link[0]}</font></a>);
+        //     }
+        //     i++;
+        //     //}catch(err){}
+        //   }
+        //   else if (subject[i] == '\n') {
+        //     subjectArrayToDisplay.push(<br />);
+        //   }
+        //   else if (subject[i] !== ']]') {
+        //     subjectArrayToDisplay.push(subject[i]);
+        //   }
+        // }
 
         for (var i = 0; i < bodyArray.length; i++) {
           if (bodyArray[i] == '[[') {
@@ -99,12 +169,33 @@ class BodyModal extends React.Component {
           }
 
         }
-        subjectArray.push(<div className="topicBody"><p><b>{subjectArrayToDisplay}<br /></b>{bodyArrayToDisplay}</p>{outerTextToDisplay}</div>);
+        subjectArray.push(<div className="topicBody"><p><b><br /></b>{bodyArrayToDisplay}</p>{outerTextToDisplay}</div>);
       });
     });
     return subjectArray;
+  //     //this.pro
   }
+toggle=(display)=>{
+  console.log("efdvdfdfds",display);
+  // var divSub=document.getElementById("subject");
+  // console.log("divv",divSub);
+  // if(divSub.style.display=="none"){
 
+  //   divSub.style.display="block";
+  // }
+  // else{
+    
+  //   divSub.style.display="none";
+  // }
+  var sub = [];
+  sub=this.getsubjectArray(display);
+  this.setState({
+    subject:sub
+
+  })
+    
+  
+}
 
   render() {
 
@@ -114,7 +205,7 @@ class BodyModal extends React.Component {
     }
     var subject = [];
     subject = this.getsubjectArray(this.props.display);
-
+     var topicname = this.getTopicNAme(this.props.display);
     // The gray background
     const backdropStyle = {
       top: 0,
@@ -148,7 +239,15 @@ class BodyModal extends React.Component {
         </div>
         <div className="myModal" style={myModalStyle}>
           <div>
-            {subject}
+          <details 
+            id={topicname}
+            onClick={()=>this.toggle(this.props.display)} 
+          >
+            <summary><font size="+1"><b>{topicname}</b></font></summary>
+        {/* <div id="subject" style="display:none">{'\n'}{subject}</div>
+       */}
+      </details>
+            {this.state.subject}
             <div>
               <button className="button3" onClick={this.props.onClose}>{this.props.button}</button>
             </div>
@@ -168,3 +267,5 @@ BodyModal.propTypes = {
 };
 
 export default BodyModal;
+
+
