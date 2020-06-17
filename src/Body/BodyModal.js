5dbbd139733d6cda707b3142e9a563178ca358da
navpreetkaur051 <65980427+navpreetkaur051@users.noreply.github.com>
2020-06-17 18:18:18 -0400

    Body Listings _Rishang

--- src/Body/BodyModal.js
+++ src/Body/BodyModal.js
@@ -1,37 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { PageViewTimer, GaUserEvent } from '../Tracking';
-
 
 class BodyModal extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      topicDisplayed: [],
-      subject:[],
-      isToggle: true,
+      topicDisplayed: []
     };
   }
-  getTopicNAme=(display)=>{
+
+  getsubjectArray = (display) => {
+    const Image = "./";
+    var subjectArray = [];
+    //kind of redendant sorry
     var topicsToDisplay = display;
 
-    var subjectArray=[];
+    //I dont think this is a word
+    const blueist = '#27AAE1';
+
+    const listItemStyle = {
+      backgroundColor: blueist,
+      fontWeight: 300,
+      borderRadius: 15,
+      width: '100%',
+      minHeight: 50,
+      margin: '3px',
+      textAlign: 'left',
+      padding: '10px',
+      color: 'white'
+    };
     
-    var topicsToDisplay = display;
-
     topicsToDisplay.forEach((topic) => {
       var bodys = topic.body;
       //var subject = body.subject;
       subjectArray.push(<div><h3>{topic.name}</h3></div>);
       var k = 0;
       bodys.forEach((body) => {
-        //var bodyArray = body.text.split(/(\[\[|\]\]|\n)/g);
+        var bodyArray = body.text.split(/(\[\[|\]\]|\n)/g);
         var subject = body.subject.split(/(\[\[|\]\]|\n)/g);
-        //var bodyArrayToDisplay = [];
+        var bodyArrayToDisplay = [];
         var subjectArrayToDisplay = [];
         var outerTextToDisplay = [];
-
+        var itemID = 0;;
+        var indexID = "item";
         for (var i = 0; i < subject.length; i++) {
           if (subject[i] == '[[') {
             var link = subject[i + 1].split(';');
@@ -41,7 +53,7 @@ class BodyModal extends React.Component {
             //try{
             if (link[0] === "image" || link[0] === "images") {
               var adress = Image + link[1].trim();
-              subjectArrayToDisplay.push(<div><img className="imageFromFolder" src={adress} alt="" /></div>);
+              subjectArrayToDisplay.push(<div key={itemID}><img className="imageFromFolder" src={adress} alt="" /></div>);
             }
             /*else if(link[1].indexOf("topic") === 0 || link[1].indexOf("topic") === 1){
               var id = topic.name + k;
@@ -58,7 +70,7 @@ class BodyModal extends React.Component {
               subjectArrayToDisplay.push(<div onClick={(idTarget) => this.togglePopUp(testId)}><font color="Yellow">{link[0]}</font><div id={testId} className="popup"><span className="popuptext"><p>{testOuterText}</p></span></div></div>);
             }*/
             else {
-              subjectArrayToDisplay.push(<a href={link[1]} target="_blank"><font color="Yellow">{link[0]}</font></a>);
+              subjectArrayToDisplay.push(<a href={link[1]} target="_blank" key={itemID}><font color="Yellow">{link[0]}</font></a>);
             }
             i++;
             //}catch(err){}
@@ -70,73 +82,6 @@ class BodyModal extends React.Component {
             subjectArrayToDisplay.push(subject[i]);
           }
         }
-        subjectArray.push(<div className="topicBody"><p><b>{subjectArrayToDisplay}<br/></b></p>{outerTextToDisplay}</div>);
-      });
-    });
-    return subjectArray;
-
-
-
-    //return topicHeading;
-  }
-
-
-  getsubjectArray = (display) => {
-    const Image = "./";
-    var subjectArray = [];
-    //kind of redendant sorry
-    var topicsToDisplay = display;
-
-    topicsToDisplay.forEach((topic) => {
-      var bodys = topic.body;
-      //var subject = body.subject;
-      //subjectArray.push(<div><h3>{topic.name}</h3></div>);
-      var k = 0;
-      bodys.forEach((body) => {
-        var bodyArray = body.text.split(/(\[\[|\]\]|\n)/g);
-        var subject = body.subject.split(/(\[\[|\]\]|\n)/g);
-        var bodyArrayToDisplay = [];
-       // var subjectArrayToDisplay = [];
-        var outerTextToDisplay = [];
-
-        // for (var i = 0; i < subject.length; i++) {
-        //   if (subject[i] == '[[') {
-        //     var link = subject[i + 1].split(';');
-        //     if (link[1] === undefined) {
-        //       link[1] = "undefined";
-        //     }
-        //     //try{
-        //     if (link[0] === "image" || link[0] === "images") {
-        //       var adress = Image + link[1].trim();
-        //       subjectArrayToDisplay.push(<div><img className="imageFromFolder" src={adress} alt="" /></div>);
-        //     }
-        //     /*else if(link[1].indexOf("topic") === 0 || link[1].indexOf("topic") === 1){
-        //       var id = topic.name + k;
-        //       k++;
-        //       var mTopic = link[1].replace("topic://", "").trim();
-        //       var outerText = this.getOuterText(mTopic);
-        //       subjectArrayToDisplay.push(<div onClick={(idTarget) => this.togglePopUp(id)}><font color="Yellow">{link[0]}</font><div id={id} className="popup"><span className="popuptext"><p>{outerText}</p></span></div></div>);
-        //     }
-        //     else if(link[1].indexOf("test") === 0 || link[1].indexOf("test") === 1){
-        //       var testId = topic.name + k;
-        //       k++;
-        //       var mTest = link[1].replace("test://", "").trim();
-        //       var testOuterText = this.getOuterText(mTest);
-        //       subjectArrayToDisplay.push(<div onClick={(idTarget) => this.togglePopUp(testId)}><font color="Yellow">{link[0]}</font><div id={testId} className="popup"><span className="popuptext"><p>{testOuterText}</p></span></div></div>);
-        //     }*/
-        //     else {
-        //       subjectArrayToDisplay.push(<a href={link[1]} target="_blank"><font color="Yellow">{link[0]}</font></a>);
-        //     }
-        //     i++;
-        //     //}catch(err){}
-        //   }
-        //   else if (subject[i] == '\n') {
-        //     subjectArrayToDisplay.push(<br />);
-        //   }
-        //   else if (subject[i] !== ']]') {
-        //     subjectArrayToDisplay.push(subject[i]);
-        //   }
-        // }
 
         for (var i = 0; i < bodyArray.length; i++) {
           if (bodyArray[i] == '[[') {
@@ -144,7 +89,7 @@ class BodyModal extends React.Component {
             try {
               if (link[0] === "image" || link[0] === "images") {
                 var adress = Image + link[1].trim();
-                bodyArrayToDisplay.push(<div><img className="imageFromFolder" id={i} src={adress} alt="" /></div>);
+                bodyArrayToDisplay.push(<div key={itemID}><img className="imageFromFolder" src={adress} alt="" /></div>);
               }
               /*else if(link[1].indexOf("topic") === 0 || link[1].indexOf("topic") === 1){
                 var id = topic.name + k;
@@ -157,7 +102,7 @@ class BodyModal extends React.Component {
                 bodyArrayToDisplay.push(<a><font color="Yellow">{link[0]}</font></a>);
               }*/
               else {
-                bodyArrayToDisplay.push(<a href={link[1]} target="_blank"><font color="Yellow">{link[0]}</font></a>);
+                bodyArrayToDisplay.push(<a href={link[1]} target="_blank" key={itemID}><font color="Yellow">{link[0]}</font></a>);
               }
               i++;
             } catch (err) { }
@@ -170,43 +115,27 @@ class BodyModal extends React.Component {
           }
 
         }
-        subjectArray.push(<div className="topicBody"><p><b><br /></b>{bodyArrayToDisplay}</p>{outerTextToDisplay}</div>);
+        indexID = indexID.concat(itemID.toString());
+        subjectArray.push(<div className="topicBody"  style={listItemStyle}>
+          <details
+            id={indexID} class="mydetailsItem">
+            <summary class="mysummaryItem"><font size="+1"> 
+              {/*<p> <b> */}
+              {subjectArrayToDisplay}
+              {/* </b> </p>*/}
+            
+            </font></summary>
+            <br />
+            {bodyArrayToDisplay}
+            {outerTextToDisplay}
+          </details>
+        </div>);
+        itemID++;
       });
     });
     return subjectArray;
-  //     //this.pro
-  }
-toggle=(display)=>{
-  // var divSub=document.getElementById("subject");
-  // console.log("divv",divSub);
-  // if(divSub.style.display=="none"){
-
-  //   divSub.style.display="block";
-  // }
-  // else{
-    
-  //   divSub.style.display="none";
-  // }
-  
-  var sub = [];
-  sub = this.getsubjectArray(display);
-  this.setState(prevState => ({
-    isToggle: !prevState.isToggle,
-  }))
-  if(!this.state.isToggle) {
-    this.setState({
-      subject: [],
-    })
-  } else {
-    this.setState({
-      subject: sub,
-    })
   }
 
-
-    
-  
-}
 
   render() {
 
@@ -216,7 +145,7 @@ toggle=(display)=>{
     }
     var subject = [];
     subject = this.getsubjectArray(this.props.display);
-     var topicname = this.getTopicNAme(this.props.display);
+
     // The gray background
     const backdropStyle = {
       top: 0,
@@ -228,15 +157,15 @@ toggle=(display)=>{
 
     // The modal "window"
     const myModalStyle = {
-      backgroundColor: '#27AAE1',
+      backgroundColor: '#808080',
       width: '90%',
       left: '0%',
       top: '10%',
       right: '0%',
-      minHeight: 300,
+      minHeight: '40%',
       margin: '0 auto',
       textAlign: 'left',
-      padding: 30,
+      padding: 20,
       color: 'white',
       overflowY: 'scroll'
     };
@@ -250,17 +179,8 @@ toggle=(display)=>{
         </div>
         <div className="myModal" style={myModalStyle}>
           <div>
-          <details 
-            id={topicname}
-            onClick={()=>this.toggle(this.props.display)} 
-          >
-            {/* {this.state.isToggle? "ON" : "OFF"} */}
-            <summary><font size="+1"><b>{topicname}</b></font></summary>
-        {/* <div id="subject" style="display:none">{'\n'}{subject}</div>
-       */}
-      </details>
-      {this.state.subject}
-            <div>
+            {subject}
+            <div className="myModalCloseButton">
               <button className="button3" onClick={this.props.onClose}>{this.props.button}</button>
             </div>
           </div>
@@ -279,5 +199,3 @@ BodyModal.propTypes = {
 };
 
 export default BodyModal;
-
-
