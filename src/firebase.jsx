d5486a3436b23227ac10ffec6b678b0841a75558
krishna1768 <kbach021@uottawa.ca>
2020-10-08 20:57:19 -0400

    changes

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -9,7 +9,9 @@ const firebaseConfig = {
   projectId: "canbewell-uottawa",
   storageBucket: "canbewell-uottawa.appspot.com",
   messagingSenderId: "813615648464",
-  };
+  appId: "1:813615648464:web:14052cf90420114be318d5",
+  measurementId: "G-935F1NQDM2"
+};
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
 }
