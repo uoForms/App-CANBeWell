3c7bc9ca890f3dba189f69771eb784b0287aac80
krishna1768 <kbach021@uottawa.ca>
2020-11-24 21:19:20 -0500

    Changed get userid function

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,11 @@ class App extends Component {
         clientId = tracker.get('clientId');
       }
     );
-    cookies.set('userID', clientId, { path: "/" });
+    if( !cookies.get('userID') ) 
+    {
+      cookies.set('userID', clientId, { path: "/" });
+    }
+      
     console.log('userid:',cookies.get('userID'))
 
     if( !cookies.get('sessionID') ) 
