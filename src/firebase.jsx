2bf42f33e7a17c1f5188a8309b14e8cb6d37a962
Ruiwen Dong <dongrw426@gmail.com>
2021-03-16 19:20:02 -0400

    Revert "Merge branch 'transgender' of https://github.com/uoForms/App-CANBeWell into transgender"
    This reverts commit 85401e92c0d9c4ca02e631431211923b9daa745f, reversing
    changes made to f263f882b4c42768aee2e23984630562fa92a44e.

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,19 +1,36 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
+import {isTransgender} from './config';
 
+if(isTransgender){
+  //connecting to Transgender database
 const firebaseConfig = {
-  apiKey: "AIzaSyA3szGH387ezJSfbaPJKs9QHzz4QYBSoFw",
-  authDomain: "export-csv-canbewell.firebaseapp.com",
-  databaseURL: "https://export-csv-canbewell.firebaseio.com",
-  projectId: "export-csv-canbewell",
-  storageBucket: "export-csv-canbewell.appspot.com",
-  messagingSenderId: "150585800085",
+  apiKey: "AIzaSyCLe-xIVjYyE9EBgQiuvsd4CyB2C7baoe4",
+  authDomain: "transgender-canbewell.firebaseapp.com",
+  databaseURL: "https://transgender-canbewell-default-rtdb.firebaseio.com",
+  projectId: "transgender-canbewell",
+  storageBucket: "transgender-canbewell-default-rtdb.appspot.com",
+  messagingSenderId: "454936296780",
+  };
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+}
+else{
+  //Connecting to Test(export-csv-canbewell) database
+  const firebaseConfig = {
+    apiKey: "AIzaSyA3szGH387ezJSfbaPJKs9QHzz4QYBSoFw",
+    authDomain: "export-csv-canbewell.firebaseapp.com",
+    databaseURL: "https://export-csv-canbewell.firebaseio.com",
+    projectId: "export-csv-canbewell",
+    storageBucket: "export-csv-canbewell.appspot.com",
+    messagingSenderId: "150585800085",
     };
- if (!firebase.apps.length) {
+  if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
   }
-
+}
 
 const db = firebase.database()
 //const auth = firebase.auth()
