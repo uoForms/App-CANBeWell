35e256669cd471fa4ffa5303e995d50a6e5b4ca0
RISHANG SHARMA <59984749+RISHANGSHARMA@users.noreply.github.com>
2021-03-18 19:57:35 -0400

    Deploy to test website
    after merging Trans to Main with istransgender=false

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // this is the setting for opening the right app.
 //Set isTransgender to false for traditinal app.
 //And set it to true for transgender settings.
-const isTransgender = true;
+const isTransgender = false;
 export {isTransgender}
