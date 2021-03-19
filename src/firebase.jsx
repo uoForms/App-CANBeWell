33e5e544dc9090b66193cc3c383d428b5330f15c
RISHANG SHARMA <59984749+RISHANGSHARMA@users.noreply.github.com>
2021-03-19 19:27:51 -0400

    Pushing For canbewell test
    istrangeder=false

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
-  //Connecting to Transgender database
+  //Connecting to test database
   const firebaseConfig = {
-    apiKey: "AIzaSyCLe-xIVjYyE9EBgQiuvsd4CyB2C7baoe4",
-    authDomain: "transgender-canbewell.firebaseapp.com",
-    databaseURL: "https://transgender-canbewell-default-rtdb.firebaseio.com",
-    projectId: "transgender-canbewell",
-    storageBucket: "transgender-canbewell-default-rtdb.appspot.com",
-    messagingSenderId: "454936296780",
+    apiKey: "AIzaSyA3szGH387ezJSfbaPJKs9QHzz4QYBSoFw",
+    authDomain: "export-csv-canbewell.firebaseapp.com",
+    databaseURL: "https://export-csv-canbewell.firebaseio.com",
+    projectId: "export-csv-canbewell",
+    storageBucket: "export-csv-canbewell.appspot.com",
+    messagingSenderId: "150585800085",
     };
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
