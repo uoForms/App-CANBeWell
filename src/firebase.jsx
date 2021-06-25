175ab7c217dcfcb76cc74ae8f837b01d14f02e43
ABHIJITH KUMAR <mail4abhijithkumar@gmail.com>
2021-06-24 20:41:45 -0400

    Deployment to Tgender website

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
-//connecting to test database
+//Connecting to transgender database
 const firebaseConfig = {
-  apiKey: "AIzaSyA3szGH387ezJSfbaPJKs9QHzz4QYBSoFw",
-  authDomain: "export-csv-canbewell.firebaseapp.com",
-  databaseURL: "https://export-csv-canbewell.firebaseio.com",
-  projectId: "export-csv-canbewell",
-  storageBucket: "export-csv-canbewell.appspot.com",
-  messagingSenderId: "150585800085",
+  apiKey: "AIzaSyCLe-xIVjYyE9EBgQiuvsd4CyB2C7baoe4",
+  authDomain: "transgender-canbewell.firebaseapp.com",
+  databaseURL: "https://transgender-canbewell-default-rtdb.firebaseio.com",
+  projectId: "transgender-canbewell",
+  storageBucket: "transgender-canbewell-default-rtdb.appspot.com",
+  messagingSenderId: "454936296780",
   };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
