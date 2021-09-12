fa82d957515bae2d63f6d68b03bc8beb512fe0b9
Songyu-Wang <wangsongyuf@gmail.com>
2021-09-11 18:57:01 -0700

    to revert

--- cypress/support/devicesTestWrapper.js
+++ cypress/support/devicesTestWrapper.js
@@ -6,26 +6,26 @@ const devices = [
     width: 1440,
     height: 900,
   },
-  {
-    model: 'Tablet Horizontal',
-    width: 1024,
-    height: 768,
-  },
-  {
-    model: 'Tablet Vertical',
-    width: 768,
-    height: 1024,
-  },
-  {
-    model: 'Phone Horizontal',
-    width: 736,
-    height: 414,
-  },
-  {
-    model: 'Phone Vertical',
-    width: 414,
-    height: 736,
-  },
+  // {
+  //   model: 'Tablet Horizontal',
+  //   width: 1024,
+  //   height: 768,
+  // },
+  // {
+  //   model: 'Tablet Vertical',
+  //   width: 768,
+  //   height: 1024,
+  // },
+  // {
+  //   model: 'Phone Horizontal',
+  //   width: 736,
+  //   height: 414,
+  // },
+  // {
+  //   model: 'Phone Vertical',
+  //   width: 414,
+  //   height: 736,
+  // },
 ];
 
 const devicesTestWrapper = (describeName, describeFunction, urls = [Cypress.config().baseUrl]) => {
