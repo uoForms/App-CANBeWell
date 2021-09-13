b87746a118af00bf317fee1ecefa101ec2756a11
Songyu-Wang <wangsongyuf@gmail.com>
2021-09-13 11:10:38 -0700

    This is a default message. I am too lazy to type anything

--- scripts/generate_cypress_test_workflow_ci_configs.py
+++ scripts/generate_cypress_test_workflow_ci_configs.py
@@ -99,7 +99,7 @@ def generate_build_text_body(locale, user):
     return generate_file(
         f"Cypress(Development Build - Text Locale - Body - {locale.capitalize()} - {user.capitalize()})",
         ["push"],
-        [cancel_previous_run_step, check_out_step, generate_curl_step('https://www.sciencedirect.com/science/article/abs/pii/S1094695019301507'), generate_curl_step("https://metisnation.ca/covid19/"),
+        [cancel_previous_run_step, check_out_step, generate_curl_step("https://metisnation.ca/covid19/"),
          generate_cypress_run_step(ci_build_json, local_host, spec=spec),
          upload_screenshot_step, upload_report_step])
 
@@ -118,7 +118,7 @@ def generate_deploy_text_body(locale, user):
     return generate_file(
         f"Cypress(Deploy Build - Text Locale - Body - {locale.capitalize()} - {user.capitalize()})",
         nightly_build,
-        [check_out_step, generate_curl_step('https://www.sciencedirect.com/science/article/abs/pii/S1094695019301507'), generate_curl_step("https://metisnation.ca/covid19/"),
+        [check_out_step, generate_curl_step("https://metisnation.ca/covid19/"),
          generate_cypress_run_step(ci_deploy_json, spec=spec),
          upload_screenshot_step, upload_report_step])
 
