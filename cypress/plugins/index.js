e198f7ac8d5abe8239f1e8259040ed531de4aebc
Songyu-Wang <wangsongyuf@gmail.com>
2021-09-29 12:20:01 -0700

    This is a default message. I am too lazy to type anything

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,12 +1,11 @@
 const failedLogTask = require('cypress-failed-log/src/failed');
 const mochawesomeReporter = require('cypress-mochawesome-reporter/plugin');
 const cypressExtends = require('@bahmutov/cypress-extends');
+const coverage = require('@cypress/code-coverage/task');
 
 module.exports = (on, config) => {
   on('task', { failed: failedLogTask() });
-  // eslint-disable-next-line global-require
-  require('@cypress/code-coverage/task')(on, config);
-  // eslint-disable-next-line no-param-reassign
+  coverage(on, config);
   mochawesomeReporter(on);
   return {
     ...config,
