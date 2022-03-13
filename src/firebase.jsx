55242c292537bf01965a6a2dececcb1a2a6e35e0
Yashovardhan Sengar <67324530+YashovardhanSengar@users.noreply.github.com>
2022-03-13 10:09:04 -0400

    Test Deployment (#501)
    * Test Deployment
    
    * Updated config for new database

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,15 +2,17 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
-//connecting to main production
+//connecting to test database
 const firebaseConfig = {
-  apiKey: "AIzaSyAPuIZi-ns_KRkpTjpnEbTnnAYGflqwbwI",
-  authDomain: "canbewell-uottawa.firebaseapp.com",
-  databaseURL: "https://canbewell-uottawa.firebaseio.com",
-  projectId: "canbewell-uottawa",
-  storageBucket: "canbewell-uottawa.appspot.com",
-  messagingSenderId: "813615648464",
-  };
+  apiKey: "AIzaSyCl4wyKnUZRt25Vmxbt7-dDoR8tsG02kSk",
+  authDomain: "canbewell-2022-test.firebaseapp.com",
+  databaseURL: "https://canbewell-2022-test-default-rtdb.firebaseio.com",
+  projectId: "canbewell-2022-test",
+  storageBucket: "canbewell-2022-test.appspot.com",
+  messagingSenderId: "114580889906",
+  appId: "1:114580889906:web:97ea5a8d3c77628032f72b",
+  measurementId: "G-WLZKR9N34N"
+};
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
 }
