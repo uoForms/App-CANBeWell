09433e30f91d2d0d49c55bd19598d71e1ad15b4a
Raha Salimi <80513097+RahaSalimi@users.noreply.github.com>
2022-04-20 19:11:10 -0400

    Mental health in production (#518)
    * new mental health
    
    new mental health
    
    * mental health button
    
    * Update .firebaserc
    
    * test
    
    * Mental Health Prod
    
    deploy mental health button to production

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,14 +4,14 @@ import 'firebase/database'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCl4wyKnUZRt25Vmxbt7-dDoR8tsG02kSk",
-  authDomain: "canbewell-2022-test.firebaseapp.com",
-  databaseURL: "https://canbewell-2022-test-default-rtdb.firebaseio.com",
-  projectId: "canbewell-2022-test",
-  storageBucket: "canbewell-2022-test.appspot.com",
-  messagingSenderId: "114580889906",
-  appId: "1:114580889906:web:97ea5a8d3c77628032f72b",
-  measurementId: "G-WLZKR9N34N"
+  apiKey: "AIzaSyDeAzPuFFw6qx2ZfTD2XUU2t0gVi4Fvi9I",
+  authDomain: "canbewell-2022-prod.firebaseapp.com",
+  databaseURL: "https://canbewell-2022-prod-default-rtdb.firebaseio.com",
+  projectId: "canbewell-2022-prod",
+  storageBucket: "canbewell-2022-prod.appspot.com",
+  messagingSenderId: "77472236262",
+  appId: "1:77472236262:web:21a39b1b2f1ba280393127",
+  measurementId: "G-16GFHELNYK"
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
