ac5806c171a24b3b69615c27f6045057e289a589
Raha Salimi <80513097+RahaSalimi@users.noreply.github.com>
2022-06-26 10:56:29 -0400

    icanbewell analytics team 2022

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,14 +4,14 @@ import 'firebase/database'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCl4wyKnUZRt25Vmxbt7-dDoR8tsG02kSk",
-  authDomain: "canbewell-2022-test.firebaseapp.com",
-  databaseURL: "https://canbewell-2022-test-default-rtdb.firebaseio.com",
-  projectId: "canbewell-2022-test",
-  storageBucket: "canbewell-2022-test.appspot.com",
-  messagingSenderId: "114580889906",
-  appId: "1:114580889906:web:97ea5a8d3c77628032f72b",
-  measurementId: "G-WLZKR9N34N"
+  apiKey: "AIzaSyDDVQc6NVLVk0SHvgxRpuFshsSA4GPGelw",
+  authDomain: "icanbewellanalytics-3f628.firebaseapp.com",
+  databaseURL: "https://icanbewellanalytics-3f628-default-rtdb.firebaseio.com",
+  projectId: "icanbewellanalytics-3f628",
+  storageBucket: "icanbewellanalytics-3f628.appspot.com",
+  messagingSenderId: "294079611734",
+  appId: "1:294079611734:web:f859f2462715e08c430cf7",
+  measurementId: "G-H5YSKQTSGM"
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
