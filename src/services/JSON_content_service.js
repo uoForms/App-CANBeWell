cee21e43157b4fce9e2371bd01bbe62ce445da88
jaikumarkaushil <jaikumarkaushil@gmail.com>
2023-08-07 11:02:01 -0400

    removed commented code - firebase storage

--- src/services/JSON_content_service.js
+++
@@ -1,8 +0,0 @@
-import { ref } from "firebase/storage";
-import { firebaseStorage } from "../firebase";
-
-// externalizing JSON files using firebase storage
-export const FilterTopicListFRRef = ref(firebaseStorage, 'json/FilterTopic-FR.json');
-export const FilterTopicListENRef = ref(firebaseStorage, 'json/FilterTopic-EN.json');
-export const TopicListFRRef = ref(firebaseStorage, 'json/HtmlTopic-FR.json');
-export const TopicListENRef = ref(firebaseStorage, 'json/HtmlTopic-EN.json');
