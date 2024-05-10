f269434742335113ccbdc3fa6dd925000ad3b42c
mooesy <pete.jin@icloud.com>
2024-05-10 02:02:21 -0400

    Minor fixs & Changed all #27AAE1 to #0089B5

--- src/Topics/TopicModal.js
+++ src/Topics/TopicModal.js
@@ -15,7 +15,7 @@ getsubjectArray=(display)=>{
     var subjectArray = [];
     var bodys = display;
     
-    const blueist = '#27AAE1';
+    const blueist = '#0089B5';
 
     const listItemStyle = {
       backgroundColor: blueist,
