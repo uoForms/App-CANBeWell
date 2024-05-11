228f942518cb8814f8729cbface6b333061056c4
Pulkit Arora <77091403+pulkitarora001@users.noreply.github.com>
2024-05-10 23:35:37 -0400

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -27,17 +27,6 @@ const firebaseConfig = {
   measurementId: "G-16GFHELNYK",
 };
 
-// For Production website
-const firebaseConfig = {
-  apiKey: "AIzaSyDeAzPuFFw6qx2ZfTD2XUU2t0gVi4Fvi9I",
-  authDomain: "canbewell-2022-prod.firebaseapp.com",
-  databaseURL: "https://canbewell-2022-prod-default-rtdb.firebaseio.com",
-  projectId: "canbewell-2022-prod",
-  storageBucket: "canbewell-2022-prod.appspot.com",
-  messagingSenderId: "77472236262",
-  appId: "1:77472236262:web:21a39b1b2f1ba280393127",
-  measurementId: "G-16GFHELNYK",
-};
 
 const app = initializeApp(firebaseConfig);
 //const auth = firebase.auth()
