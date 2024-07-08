3e4d1a8869c97ddaf16f8fa064bbd51d1c50abaa
ShaikWasef <wasefshaik@gmail.com>
2024-07-08 17:14:27 -0400

    feature : Add final links of survey

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const frenchForm = "https://redcap.link/jszxzbh7";
-export const englishForm = "https://redcap.link/ne6ivfez";
+export const frenchForm = "https://redcap.montfort.on.ca/redcap/surveys/?s=K3NPTWMW93FTXMRL";
+export const englishForm = "https://redcap.montfort.on.ca/redcap/surveys/?s=NYA8PXRWHYY83DLT";
